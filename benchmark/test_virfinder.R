309ab5f507b057a76f21df167330ff61d6acf726
Aly Osama <alyosamah@gmail.com>
2018-09-18 02:13:41 -0600

    update virFinder Benchmark

--- benchmark/test_virfinder.R
+++ benchmark/test_virfinder.R
@@ -5,33 +5,44 @@
 #biocLite("qvalue")
 #install.packages("E:/masters/benchmark/virfinder/VirFinder/windows/VirFinder_1.1.zip", repos = NULL, type="source")  
 
+#install.packages("doParallel") 
 
 library(VirFinder)
+library(foreach)
+library(doParallel)
 
-fna_dir <- file.path("E:/masters/data/3-fragments/fna")
-output_dir <- file.path("E:/masters/benchmark/results")
+fna_dir <- file.path("../../data/3-fragments/fna")
+output_dir <- file.path("../../benchmark/results")
 
 
-run_virFinder <- function(testPath) {
-  ptm <- proc.time()
+run_virFinder <- function(test_file) {
+  cat("Current working file: ", test_file,'\n')
+  testPath  <- file.path(fna_dir,test_file)
   ## (2) prediction
-  predResult <- VF.pred(testPath)
+  capture.output(predResult <- VF.pred(testPath), file='/dev/null')
+
+  
   #### (2.1) sort sequences by p-value in ascending order
   predResult <- predResult[order(predResult$pvalue),]
   
-  out_path <- file.path(output_dir,paste(testPath, "csv", sep="."))
-  write.csv(file=out_path, x=predResult)
-  proc.time() - ptm
-  
+  out_path <- file.path(output_dir,paste(test_file, "csv", sep="."))
+  write.csv(file=out_path, x=predResult)  
+  cat("Finished file: ", test_file,'\n')
 }
 
-for(test_file in list.files(fna_dir)){
-  if(grepl('test',test_file)){
-    testPath  <- file.path(fna_dir,test_file)
-    run_virFinder(testPath)
 
+print('Start VirFinder')
+no_cores <- detectCores() - 1
+registerDoParallel(cores=no_cores)
+getDoParWorkers()
+
+foreach(test_file = list.files(fna_dir)) %dopar% {
+  if(grepl('test',test_file)){
+    system.time(run_virFinder(test_file))
   }
 }
+
+
 
 
   
--- benchmark/train_virfinder.R
+++ benchmark/train_virfinder.R
@@ -11,17 +11,17 @@ library(VirFinder)
 
 ## (1) train the model using user's database
 #### (1.1) specifiy the fasta files of the training contigs, one for virus and one for prokaryotic hosts
-trainFaFileHost <- system.file("data", "host.fa", package="VirFinder")
-trainFaFileVirus <- system.file("data", "virus.fa", package="VirFinder")
+trainFaFileHost <- file.path("../../data/2-train_test/non_viral_train.fna")
+trainFaFileVirus <- file.path("../../data/2-train_test/viral_train.fna")
 
 #### (1.2) specify the directory where the trained model will be saved to, and the name of the model
-userModDir <- file.path(find.package("VirFinder"))
-userModName <- "virFinderModel"
+userModDir <- file.path('../../benchmark/model')
+userModName <- "virnetDataModel"
 
 ## (2) train the model using user's database
-w <- 4 # the length of the k-tuple word
-VF.trainModUser <- VF.train.user(trainFaFileHost, trainFaFileVirus, userModDir,
-                                 userModName, w, equalSize=TRUE)
+w <- 8 # the length of the k-tuple word
+VF.trainModUser <- VF.train.user(trainFaFileHost, trainFaFileVirus, userModDir, userModName, w, equalSize=TRUE)
+
 ## (3) load the trained model based on user's database
-modFile <- list.files(userModDir, userModName, full.names=TRUE)
-load(modFile)
+#modFile <- list.files(userModDir, userModName, full.names=TRUE)
+#load(modFile)
