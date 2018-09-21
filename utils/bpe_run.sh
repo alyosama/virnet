a673408aac56c301f8330f41fdf1bd9c22cbb5f4
Aly Osama <alyosamah@gmail.com>
2018-09-21 10:59:26 -0600

    Adding other fragments with BPE

--- utils/bpe_run.sh
+++ utils/bpe_run.sh
@@ -1,16 +1,22 @@
+#BSUB -J BPE_TEST
+#BSUB -n 32
+#BSUB -R "span[hosts=1]"
+#BSUB -W 99:99
+#BSUB -o /home/alyabdel/workdir/test_%J.out
+#BSUB -e /home/alyabdel/workdir/test_%J.err
 
-N=500
 num_operations=50000
+for N in 100 300 500 1000 5000; do
+    echo "Strating N=$N and num_operations=$num_operations"
 
-echo "Strating N=$N and num_operations=$num_operations"
+    echo "Cleaning non_viral headers"
+    sed -e '/^>/ d'  ../../data/3-fragments/fna/non_viral_train.fna_$N.fna > ../../work_space/non_viral_train.fna_$N.txt
+    echo "Cleaning viral headers"
+    sed -e '/^>/ d'  ../../data/3-fragments/fna/viral_train.fna_$N.fna > ../../work_space/viral_train.fna_$N.txt
 
-echo "Cleaning non_viral headers"
-sed -e '/^>/ d'  ../../data/3-fragments/fna/non_viral_train.fna_$N.fna > ../../work_space/non_viral_train.fna_$N.txt
-echo "Cleaning viral headers"
-sed -e '/^>/ d'  ../../data/3-fragments/fna/viral_train.fna_$N.fna > ../../work_space/viral_train.fna_$N.txt
+    echo "Combine viral and non viral files"
+    cat ../../work_space/non_viral_train.fna_$N.txt ../../work_space/viral_train.fna_$N.txt > ../../work_space/train.fna_$N.txt
 
-echo "Combine viral and non viral files"
-cat ../../work_space/non_viral_train.fna_$N.txt ../../work_space/viral_train.fna_$N.txt > ../../work_space/train.fna_$N.txt
-
-echo "Run BPE on the file"
-../../subword-nmt/learn_bpe.py -s $num_operations < ../../work_space/train.fna_$N.txt > train_codes.fna_$N.txt
+    echo "Run BPE on the file"
+    ../../subword-nmt/learn_bpe.py -s $num_operations < ../../work_space/train.fna_$N.txt > ../../work_space/train_codes.fna_$N.txt &
+done 
